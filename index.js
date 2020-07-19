7f23be7e3781b3d2eb255185d039e5d8cd67fa94
meenusv <66123963+meenusv@users.noreply.github.com>
2020-07-20 00:35:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from 'App';
+import 'index.css';
 
 ReactDOM.render(
-  <App></App>,
+  <App />,
   document.getElementById('root')
 );
